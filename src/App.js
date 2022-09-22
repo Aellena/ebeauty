0d9f98b7197d554886ee9a7dd86b5c01023c099c
Christine Yoo <cyoo@zixcorp.com>
2022-09-21 21:32:00 -0500

    refactored categories container in app to separate component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,8 @@
-import './categories.styles.scss'
+import Directory from './components/directory/directory.component';
 
 const App = () => {
-
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-      imgSrc: ''
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-      imgSrc: ''
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-      imgSrc: ''
-    },
-    {
-      id: 4,
-      title: 'Womens',
-      imgSrc: ''
-    },
-    {
-      id: 5,
-      title: 'Mens',
-      imgSrc: ''
-    },
-  ];
-
   return (
-    <div className='categories-container'>
-      {categories.map(({title, id}) => ( // paren is single line return instead of curly
-        <div key={id} className='category-container'>
-          <div className='background-image' />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-
-    </div>
+    <Directory />
   );
 };
 
